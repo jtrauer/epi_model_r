03ae7ab11208008eb1a42c921f4543d9a0749b10
jtrauer <james.trauer@monash.edu>
2018-09-02 22:04:29 +1000

    Move fixed flows from vector to data frame structure

--- epi_model_r.R
+++ epi_model_r.R
@@ -2,26 +2,33 @@
 library(deSolve)
 
 # generic model builder function
+fixed_flows <- 
+  data.frame(flow_name=character(0),
+             from_compartment=character(0),
+             to_compartment=character(0))
+
+infection_flows <- matrix(nrow = 0, ncol = 3)
+
 make_epi_model <- 
   function(compartment_names, infection_flow, fixed_flows) {
   epi_model_function <- 
     function(time, compartment_values, parameters) {
+      
+    # initialise ode equations to zero
     ode_equations <- rep(0, length(compartment_names))
     
     # apply fixed flows
-    for (i in seq(dim(fixed_flows)[1])) {
-      from_compartment <- match(fixed_flows[i, 2], compartment_names)
-      to_compartment <- match(fixed_flows[i, 3], compartment_names)
-      net_flow <- parameters[fixed_flows[i, 1]] * 
+    for (i in seq(length(fixed_flows))) {
+      from_compartment <- match(fixed_flows$from_compartment[i], compartment_names)
+      to_compartment <- match(fixed_flows$to_compartment[i], compartment_names)
+      net_flow <- parameters[as.character(fixed_flows$flow_name[i])] * 
         compartment_values[from_compartment]
-      ode_equations[from_compartment] <- 
-        ode_equations[from_compartment] - net_flow
-      ode_equations[to_compartment] <-
-        ode_equations[to_compartment] + net_flow
+      ode_equations[from_compartment] <- ode_equations[from_compartment] - net_flow
+      ode_equations[to_compartment] <- ode_equations[to_compartment] + net_flow
     }
     
     # apply the infection flow
-    for (i in seq(dim(fixed_flows)[1])) {
+    for (i in seq(dim(infection_flows)[1])) {
       infectious_compartment <- match("infectious", compartment_names)
       from_compartment <- match(infection_flow[i, 2], compartment_names)
       to_compartment <- match(infection_flow[i, 3], compartment_names)
--- sir_runner.R
+++ sir_runner.R
@@ -6,9 +6,12 @@ parameters <- c(beta = 400, recovery = 365 / 13, immunity_wane = 100)
 compartments <- c("susceptible", "infectious", "recovered")
 times <- seq(from=0, to=60/365, by=1/365)
 initial_conditions <- c(susceptible=0.999, infectious=0.001, recovered=0)
-fixed_flows <- matrix(nrow = 0, ncol = 3)
-infection_flows <- matrix(nrow = 0, ncol = 3)
-fixed_flows <- rbind(fixed_flows, c("recovery", "infectious", "recovered"))
+fixed_flows <- rbind(
+  fixed_flows,
+  data.frame(flow_name="recovery",
+             from_compartment="infectious",
+             to_compartment="recovered"))
+
 infection_flows <- rbind(infection_flows,c("beta", "susceptible", "infectious"))
 
 epi_model <- make_epi_model(compartments, infection_flows, fixed_flows)
