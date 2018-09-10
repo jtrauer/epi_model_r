5312b222c91f6507b067a5694fc6d2d203003835
jtrauer <james.trauer@monash.edu>
2018-09-10 10:05:01 +1000

    Object orient most functions

--- epi_model_r.R
+++ epi_model_r.R
@@ -1,19 +1,11 @@
 
 library(deSolve)
+library(R6)
 
 # this file contains the main model builder function, that is intended to be agnostic to the type and
 # complexity of model that the user wants to build
-# all instructions on what the characteristics of the model are are separated out to a different file
+# all instructions on what the characterSistics of the model are are separated out to a different file
 # that calls/sources this one
-
-# initialise data frames to store flows by type
-create_flows_dataframe <- function() {
-  data.frame(flow_name=character(0), 
-             from_compartment=character(0),
-             to_compartment=character(0))
-}
-fixed_flows <- create_flows_dataframe()
-infection_flows <- create_flows_dataframe()
 
 # increment a numbered element of a vector
 increment_vector_element <- function(vector, number, increment) {
@@ -21,93 +13,114 @@ increment_vector_element <- function(vector, number, increment) {
   vector
 }
 
-# initialise compartments to zero values
-initialise_compartments <- function(compartment_names) {
-  if(!(is.character(compartment_names))) {
-    stop("compartment names are not character")
-  }
-  initial_conditions <- numeric(length(compartment_names))
-  initial_conditions <- setNames(inits, compartment_names)
-}
-
-# set a compartment's starting value to a positive number (having previous set to zero)
-set_compartment_start_value <- function(initial_conditions, compartment_name, value) {
-  if(!(compartment_name %in% names(initial_conditions))) {
-    stop("compartment name not found in compartment list")
-  }
-  if(!(is.numeric(value))) {
-    stop("requested starting compartment value is not numeric")
-  }
-  if(value < 0) {
-    stop("requested starting compartment value is negative")
-  }
-  initial_conditions[compartment_name] <- value
-  initial_conditions
-}
-
-# make initial condition values up to starting total (default being 1)
-make_initial_conditions_sum_to_total <- function(initial_conditions, total=1,
-                                                 starting_name="susceptible") {
-  initial_conditions <- set_compartment_start_value(
-    initial_conditions, starting_name, total - sum(initial_conditions))
-}
-
-# define functions to add flows to model
-add_flow <- function(flow_frame, flow_name, from_compartment, to_compartment,
-                     parameters, compartments) {
-  if(!(flow_name %in% names(parameters))) {
-    stop("flow name not found in parameter list")
-  }
-  if(!(from_compartment %in% compartments)) {
-    stop("from compartment name not found in compartment list")
-  }
-  if(!(to_compartment %in% compartments)) {
-    stop("to compartment name not found in compartment list")
-  }
-  flow_frame <- rbind(
-    flow_frame,
-    data.frame(flow_name=flow_name,
-               from_compartment=from_compartment,
-               to_compartment=to_compartment)
+# define model object
+EpiModel <- R6Class(
+  "EpiModel",
+  public = list(
+    parameters = list(),
+    compartments = list(),
+    flows = list(),
+    initial_conditions = list(),
+    infectious_compartment = NULL,
+    initialize = function(parameters = NULL, compartments = NULL,
+                          infectious_compartment = "infectious") {
+      self$parameters <- parameters
+      self$compartments <- compartments
+      self$infectious_compartment <- infectious_compartment
+    },
+    
+    # initialise compartments to zero values
+    initialise_compartments = function() {
+      if(!(is.character(self$compartments))) {
+        stop("compartment names are not character")
+      }
+      self$initial_conditions <- numeric(length(self$compartments))
+      self$initial_conditions <- setNames(inits, self$compartments)
+    },
+    
+    # populate compartments with a starting value
+    set_compartment_start_value = function(compartment_name, value) {
+      if(!(compartment_name %in% names(self$initial_conditions))) {
+        stop("compartment name not found in compartment list")
+      }
+      if(!(is.numeric(value))) {
+        stop("requested starting compartment value is not numeric")
+      }
+      if(value < 0) {
+        stop("requested starting compartment value is negative")
+      }
+      self$initial_conditions[compartment_name] <- value
+      self$initial_conditions
+    },
+    
+    # make initial condition values up to starting total (default being 1)
+    make_initial_conditions_sum_to_total = function(total=1, starting_name="susceptible") {
+      self$initial_conditions <- self$set_compartment_start_value(
+        starting_name, total - sum(self$initial_conditions))
+    },
+    
+    # define functions to add flows to model
+    add_flow = function(flow_type, flow_name, from_compartment, to_compartment) {
+      if(!(flow_name %in% names(self$parameters))) {
+        stop("flow name not found in parameter list")
+      }
+      if(!(from_compartment %in% self$compartments)) {
+        stop("from compartment name not found in compartment list")
+      }
+      if(!(to_compartment %in% self$compartments)) {
+        stop("to compartment name not found in compartment list")
+      }
+      self$flows[[flow_type]] <- rbind(
+        self$flows[[flow_type]],
+        data.frame(flow_name=flow_name,
+                   from_compartment=from_compartment,
+                   to_compartment=to_compartment)
+      )
+    },
+    
+    # add the infection flow to the odes
+    apply_infection_flow = function(
+      ode_equations, compartment_values) {
+      for (f in 1: nrow(self$flows$infection_flows)) {
+        flow <- self$flows$infection_flows[f,]
+        infectious_compartment <- match(self$infectious_compartment, self$compartments)
+        from_compartment <- match(flow$from_compartment, self$compartments)
+        net_flow <- self$parameters[flow$flow_name] *
+          compartment_values[from_compartment] *
+          compartment_values[infectious_compartment]
+        ode_equations <-
+          increment_vector_element(ode_equations, from_compartment, -net_flow)
+        ode_equations <-
+          increment_vector_element(ode_equations,
+                                   match(flow$to_compartment, self$compartments),
+                                   net_flow)
+      }
+      ode_equations
+    },
+    
+    # add a fixed flow to the odes
+    apply_fixed_flow =
+      function(ode_equations, compartment_values) {
+        for (f in 1: nrow(self$flows$fixed_flows)) {
+          flow <- self$flows$fixed_flows[f,]
+          from_compartment <- match(flow$from_compartment, self$compartments)
+          net_flow <- self$parameters[as.character(flow$flow_name)] *
+            compartment_values[from_compartment]
+          ode_equations <-
+            increment_vector_element(ode_equations, from_compartment, -net_flow)
+          ode_equations <-
+            increment_vector_element(ode_equations,
+                                     match(flow$to_compartment, self$compartments),
+                                     net_flow)
+        }
+        ode_equations
+      }
   )
-}
-
-apply_infection_flow <- 
-  function(ode_equations, compartment_names, compartment_values, parameters,
-           flows, infectious_compartment) {
-  for (f in 1: nrow(flows)) {
-    flow <- flows[f,]
-    infectious_compartment <- match("infectious", compartment_names)
-    from_compartment <- match(flow$from_compartment, compartment_names)
-    net_flow <- parameters[flow$flow_name] * 
-      compartment_values[from_compartment] * 
-      compartment_values[infectious_compartment]
-    ode_equations <- increment_vector_element(ode_equations, from_compartment, -net_flow)
-    ode_equations <- increment_vector_element(ode_equations, 
-                                              match(flow$to_compartment, compartment_names), 
-                                              net_flow)
-  }
-  ode_equations
-}
-
-apply_fixed_flow <- 
-  function(ode_equations, compartment_names, compartment_values, parameters, flows) {
-  for (f in 1: nrow(flows)) {
-    flow <- flows[f,]
-    from_compartment <- match(flow$from_compartment, compartment_names)
-    net_flow <- parameters[as.character(flow$flow_name)] * 
-      compartment_values[from_compartment]
-    ode_equations <- increment_vector_element(ode_equations, from_compartment, -net_flow)
-    ode_equations <- increment_vector_element(ode_equations, 
-                                              match(flow$to_compartment, compartment_names), 
-                                              net_flow)
-  }
-  ode_equations
-}
+)
 
 # model builder function
 make_epi_model <- 
-  function(compartment_names, infection_flows, fixed_flows) {
+  function(model, compartment_names, infection_flows, fixed_flows) {
   epi_model_function <- 
     function(time, compartment_values, parameters) {
       
@@ -115,20 +128,20 @@ make_epi_model <-
     ode_equations <- rep(0, length(compartment_names))
     
     # apply flows
-    ode_equations <- apply_fixed_flow(
-      ode_equations, compartment_names, compartment_values, parameters, fixed_flows)
-    ode_equations <-apply_infection_flow(
-      ode_equations, compartment_names, compartment_values, parameters, 
-      infection_flows, infectious_compartment)
+    ode_equations <- model$apply_fixed_flow(
+      ode_equations, compartment_values)
+    ode_equations <- model$apply_infection_flow(
+      ode_equations, compartment_values)
     list(ode_equations)
   }
 }
 
 # run model
-run_model <- function (compartments, infection_flows, fixed_flows) {
-  epi_model <- make_epi_model(compartments, infection_flows, fixed_flows)
+run_model <- function (model, compartments, infection_flows, fixed_flows) {
+  epi_model <- make_epi_model(model, compartments, infection_flows, fixed_flows)
   out <- as.data.frame(
     ode(func=epi_model, y=initial_conditions, times=times, parms=parameters
     )
   )  
 }
+
--- sir_runner.R
+++ sir_runner.R
@@ -1,22 +1,22 @@
 
-source("epi_model_r.r")
+source("epi_model_r.R")
 
 # an example script to call the generic model builder file that constructs a compartmental model
 # from the instructions contained in this file
 
-# request model features
-parameters <- c(beta=400, recovery=365/13)
-times <- seq(from=0, to=60/365, by=1/365)
-initial_conditions <- initialise_compartments(c("susceptible", "infectious", "recovered"))
-initial_conditions <- set_compartment_start_value(initial_conditions, "infectious", 0.001)
-initial_conditions <- make_initial_conditions_sum_to_total(initial_conditions)
-
-fixed_flows <- add_flow(
-  fixed_flows, "recovery", "infectious", "recovered", parameters, compartments)
-infection_flows <- add_flow(
-  infection_flows, "beta", "susceptible", "infectious", parameters, compartments)
+sir_model <- EpiModel$new(c(beta=400, recovery=365/13), 
+                          c("susceptible", "infectious", "recovered"))
+sir_model$initialise_compartments()
+sir_model$set_compartment_start_value("infectious", 0.001)
+sir_model$make_initial_conditions_sum_to_total()
+sir_model$add_flow("fixed_flows", "recovery", "infectious", "recovered")
+sir_model$add_flow("infection_flows", "beta", "susceptible", "infectious")
 
 # run model
-outputs <- run_model(compartments, infection_flows, fixed_flows)
+outputs <- run_model(sir_model,
+                     sir_model$compartments,
+                     sir_model$flows$infection_flows,
+                     sir_model$flows$fixed_flows)
 plot(outputs$time, outputs$infectious)
+
 
