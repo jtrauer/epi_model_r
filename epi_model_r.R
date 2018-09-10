ddf421e19ec7fe9e98ac9ad7153489a098b17840
jtrauer <james.trauer@monash.edu>
2018-09-10 21:51:26 +1000

    Final round of cleaning to both files of code

--- epi_model_r.R
+++ epi_model_r.R
@@ -2,10 +2,12 @@
 library(deSolve)
 library(R6)
 
-# this file contains the main model builder function, that is intended to be agnostic to the type and
-# complexity of model that the user wants to build
-# all instructions on what the characterSistics of the model are are separated out to a different file
-# that calls/sources this one
+# this file contains the main model builder function, that is intended to be agnostic
+# to the type and complexity of model that the user wants to build
+# all instructions on what the characteristics of the model are are separated out to a
+# file that calls/sources this one
+
+# static functions
 
 # increment a numbered element of a vector
 increment_vector_element <- function(vector, number, increment) {
@@ -13,30 +15,34 @@ increment_vector_element <- function(vector, number, increment) {
   vector
 }
 
-# define model object
+# main model object
 EpiModel <- R6Class(
   "EpiModel",
   public = list(
-    parameters = list(),
     compartments = list(),
-    flows = list(),
     initial_conditions = list(),
+    flows = list(),
     infectious_compartment = NULL,
+    parameters = list(),
     outputs = NULL,
     times = NULL,
-    initialize = function(parameters = NULL, compartments = NULL, times = NULL,
-                          infectious_compartment = "infectious") {
+    initialize = function(parameters=NULL, compartments=NULL, times=NULL,
+                          infectious_compartment="infectious") {
+      if(!(is.numeric(parameters))) {stop("parameter values are not numeric")}
       self$parameters <- parameters
+      if(!(is.character(compartments))) {stop("compartment names are not character")}
       self$compartments <- compartments
+      if(!(is.character(infectious_compartment))) {
+        stop("infectious compartment name is not character")
+      }
       self$infectious_compartment <- infectious_compartment
+      if(!(is.numeric(times))) {stop("time values are not numeric")}
       self$times <- times
+      self$initialise_compartments()
     },
     
     # initialise compartments to zero values
     initialise_compartments = function() {
-      if(!(is.character(self$compartments))) {
-        stop("compartment names are not character")
-      }
       self$initial_conditions <- numeric(length(self$compartments))
       self$initial_conditions <- setNames(self$initial_conditions, self$compartments)
     },
@@ -44,7 +50,7 @@ EpiModel <- R6Class(
     # populate compartments with a starting value
     set_compartment_start_value = function(compartment_name, value) {
       if(!(compartment_name %in% names(self$initial_conditions))) {
-        stop("compartment name not found in compartment list")
+        stop("starting compartment name not found in compartment list")
       }
       if(!(is.numeric(value))) {
         stop("requested starting compartment value is not numeric")
@@ -73,24 +79,21 @@ EpiModel <- R6Class(
       if(!(to_compartment %in% self$compartments)) {
         stop("to compartment name not found in compartment list")
       }
-      self$flows[[flow_type]] <- rbind(
-        self$flows[[flow_type]],
-        data.frame(flow_name=flow_name,
-                   from_compartment=from_compartment,
-                   to_compartment=to_compartment)
+      self$flows[[flow_type]] <- rbind(self$flows[[flow_type]],
+                                       data.frame(flow_name=flow_name,
+                                                  from_compartment=from_compartment,
+                                                  to_compartment=to_compartment)
       )
     },
     
     # apply the infection flow to odes
-    apply_infection_flow = function(
-      ode_equations, compartment_values) {
+    apply_infection_flow = function(ode_equations, compartment_values) {
       for (f in 1: nrow(self$flows$infection_flows)) {
         flow <- self$flows$infection_flows[f,]
         infectious_compartment <- match(self$infectious_compartment, self$compartments)
         from_compartment <- match(flow$from_compartment, self$compartments)
         net_flow <- self$parameters[flow$flow_name] *
-          compartment_values[from_compartment] *
-          compartment_values[infectious_compartment]
+          compartment_values[from_compartment] * compartment_values[infectious_compartment]
         ode_equations <-
           increment_vector_element(ode_equations, from_compartment, -net_flow)
         ode_equations <-
@@ -120,10 +123,8 @@ EpiModel <- R6Class(
       },
 
     # create derivative function
-    make_model_function =
-      function() {
-        epi_model_function <- 
-          function(time, compartment_values, parameters) {
+    make_model_function = function() {
+        epi_model_function <- function(time, compartment_values, parameters) {
             
             # initialise to zero for each compartment
             ode_equations <- rep(0, length(self$compartments))
@@ -135,7 +136,7 @@ EpiModel <- R6Class(
           }
       },
 
-    # integrate the model odes  
+    # integrate model odes  
     run_model = function () {
       self$outputs <- as.data.frame(ode(
         func=self$make_model_function(), y=self$initial_conditions, times=self$times)
--- sir_runner.R
+++ sir_runner.R
@@ -4,10 +4,9 @@ source("epi_model_r.R")
 # an example script to call the generic model builder file that constructs a compartmental model
 # from the instructions contained in this file
 
-sir_model <- EpiModel$new(c(beta=400, recovery=365/13), 
+sir_model <- EpiModel$new(c(beta=400, recovery=365/13),
                           c("susceptible", "infectious", "recovered"),
                           seq(from=0, to=60/365, by=1/365))
-sir_model$initialise_compartments()
 sir_model$set_compartment_start_value("infectious", 0.001)
 sir_model$make_initial_conditions_to_total()
 sir_model$add_flow("fixed_flows", "recovery", "infectious", "recovered")
