6532923c1b4d110899dfb9881638a2d3d3fe60e9
James Trauer <james.trauer@monash.edu>
2018-12-14 16:06:21 +1100

    ...actually include changes to master script

--- sir_runner.R
+++ sir_runner.R
@@ -16,15 +16,18 @@ sir_model <- EpiModel$new(c(beta=400, recovery=365/13),
                           list("infectious"=0.001),
                           list(c("standard_flows", "recovery", "infectious", "recovered"),
                                c("infection_density", "beta", "susceptible", "infectious")))
-sir_model$implement_stratification("hiv", 2, c(),
-                                   list(recovery=list(adjustments=list("1"=0.7, "2"=0.5))))
-# sir_model$implement_stratification("risk", 2, c("recovered"), list(recovery.hiv_positive=list(adjustments=list("1"=1.5, "2"=1))))
+sir_model$implement_stratification("hiv", c("negative", "positive"), c(),
+                                   list(recovery=list(adjustments=list("negative"=0.7, "positive"=0.5))))
+sir_model$implement_stratification("risk", 2, c("recovered"), list(recovery=list(adjustments=list("1"=1.5, "2"=1)),
+                                                                   recovery0hiv_negative=list(adjustments=list("1"=2, "2"=4))))
 
 # sir_model$add_time_variant("recovery", create_arbitrary_time_variant_function)
 
-sir_model$report_model_structure()
+# sir_model$report_model_structure()
 
 sir_model$run_model()
+
+
 
 interpreter <- ModelInterpreter$new(sir_model)
 interpreter$plot_compartment("infectious")
