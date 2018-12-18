bfb3c889aff2b2bc81e818c77959c2132eee59e3
Romain Ragonnet <romain.ragonnet@burnet.edu.au>
2018-12-18 14:34:35 +0100

    Update method name from "implement_stratification" to "stratify" in test file

--- sir_runner.R
+++ sir_runner.R
@@ -16,9 +16,9 @@ sir_model <- EpiModel$new(c(beta=400, recovery=365/13),
                           list("infectious"=0.001),
                           list(c("standard_flows", "recovery", "infectious", "recovered"),
                                c("infection_density", "beta", "susceptible", "infectious")))
-sir_model$implement_stratification("hiv", c("negative", "positive"), c(),
+sir_model$stratify("hiv", c("negative", "positive"), c(),
                                    list(recovery=list(adjustments=list("negative"=0.7, "positive"=0.5))))
-sir_model$implement_stratification("risk", 2, c("recovered"), list(recovery=list(adjustments=list("1"=1.5, "2"=1)),
+sir_model$stratify("risk", 2, c("recovered"), list(recovery=list(adjustments=list("1"=1.5, "2"=1)),
                                                                    recovery0hiv_negative=list(adjustments=list("1"=2, "2"=4))))
 
 # sir_model$add_time_variant("recovery", create_arbitrary_time_variant_function)
