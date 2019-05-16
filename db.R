31484372c616d6d27b86e7cadd34b11422f00c32
swas0001 <swasnik@gmail.com>
2019-05-16 10:59:00 +1000

    storing model outputs to sqlite DB

--- db.R
+++ db.R
@@ -1,10 +1,25 @@
 library(RSQLite)
 conn <- dbConnect(SQLite(), "summer.db")
 
-notification_2019 = read.csv('xls/notifications_2019.csv', header = TRUE)
+filenames = list.files('xls')
 
-dbWriteTable(conn, "notification_2019", notification_2019, row.names = FALSE)
+for (fname in filenames) {
+  tablename = str_remove(fname, '.csv')
+  print(tablename)
+  df = read.csv(file.path('xls', fname), header = TRUE)
+  dbWriteTable(conn, tablename, df, row.names = FALSE)
+}
 
-head(dbReadTable(conn, "notification_2019"))
+#notification_2019 = read.csv('xls/notifications_2019.csv', header = TRUE)
 
+#dbWriteTable(conn, "notification_2019", notification_2019, row.names = FALSE)
+
+#head(dbReadTable(conn, "notification_2019"))
+
+
+conn <- dbConnect(SQLite(), "summer.db")
+source('sir_runner.R')
+sir_model$compartment_values
+outputs  = sir_model$outputs
+dbWriteTable(conn, "model_outputs", outputs, row.names =FALSE )
 dbDisconnect(conn)
