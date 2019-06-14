68f1f72198564f3dd4fa6d5e5fb98ac4368096dd
James Trauer <james.trauer@monash.edu>
2019-06-14 14:43:17 +1000

    write cleaner code to facilitate getting latency parameters into model quickly

--- tb_model.R
+++ tb_model.R
@@ -12,6 +12,28 @@ add_vtp_latency_parameters = function(.parameters, change_time_unit=365.25) {
   .parameters <- c(.parameters, vtp_latency_parameters)
 }
 
+get_age_specific_latency_parameters = function(.parameters, change_time_unit=365.25) {
+  # get the age-specific latency parameters estimated by Ragonnet et al
+  age_stratified_parameters = list(early_progression=c("0"=6.6e-3, "5"=2.7e-3, "15"=2.7e-4),
+                                   stabilisation=c("0"=1.2e-2, "5"=1.2e-2, "15"=5.4e-3),
+                                   late_progression=c("0"=1.9e-11, "5"=6.4e-6, "15"=3.3e-6))
+  return(age_stratified_parameters)
+}
+
+
+get_all_age_specific_latency_parameters = function(change_time_unit=365.25) {
+  # collate all the latency parameters together from the previous function
+  age_specific_parameters <- get_age_specific_latency_parameters()
+  revised_parameters <- list()
+  for (parameter in names(age_specific_parameters)) {
+    revised_parameters[[parameter]] <- list(adjustments=c(), overwrite=names(age_specific_parameters[[parameter]]))
+    for (age in names(age_specific_parameters[[parameter]]))
+      revised_parameters[[parameter]]$adjustments[[age]] <- age_specific_parameters[[parameter]][[age]] * change_time_unit
+  }
+  return(revised_parameters)
+}
+
+
 add_standard_latency_flows = function(.flows) {
   # adds our standard latency flows to the list of flows to be implemented in the model
   .flows <- c(.flows, list(c("standard_flows", "early_progression", "early_latent", "infectious"),
@@ -19,11 +41,10 @@ add_standard_latency_flows = function(.flows) {
                            c("standard_flows", "late_progression", "late_latent", "infectious")))
 }
 
-
 # main section of code to run
 case_fatality_rate <- 0.4
 untreated_disease_duration <- 3
-parameters <- list(beta = 50.0,
+parameters <- list(beta = 10.0,
                    recovery = case_fatality_rate / untreated_disease_duration,
                    infect_death = (1.0 - case_fatality_rate) / untreated_disease_duration,
                    universal_death_rate = 1.0 / 50.0)
@@ -39,6 +60,9 @@ flows <- add_standard_latency_flows(flows)
 tb_model <- StratifiedModel$new(
   times, c("susceptible", "early_latent", "late_latent", "infectious", "recovered"), c(infectious=1e-3),
   parameters, flows, birth_approach="replace_deaths")
+
+
+tb_model$stratify("age", c(5, 15), c(), get_all_age_specific_latency_parameters())
 
 tb_model$run_model()
 
