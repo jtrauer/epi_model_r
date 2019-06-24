49258166948a818d9fecf6e81cc1e510e6a05912
James Trauer <james.trauer@monash.edu>
2019-06-24 10:30:41 +1000

    move all summer_python files to summer directory

---
+++ db.py
@@ -0,0 +1,96 @@
+
+from sqlalchemy import create_engine
+import pandas as pd
+import glob
+
+
+class InputDB:
+    """
+    methods for loading input xls files
+    """
+
+    def __init__(self, database_name="Inputs.db"):
+        """
+        initialise sqlite database
+        """
+        self.database_name = database_name
+        self.engine = create_engine("sqlite:///" + database_name, echo=False)
+
+    def load_csv(self, input_path="xls/*.csv"):
+        """
+        load CSVs from input_path
+        """
+        csv_file_list = glob.glob(input_path)
+        for filename in csv_file_list:
+            data_frame = pd.read_csv(filename)
+            data_frame.to_sql(filename.split("\\")[1].split(".")[0], con=self.engine, if_exists="replace")
+
+    def load_xlsx(self, input_path="xls/*.xlsx"):
+        """
+        load xlsx from input_path
+        """
+        excel_file_list = glob.glob(input_path)
+        available_sheets \
+            = ["default_constants", "country_constants", "default_programs", "country_programs", "bcg_2014", "bcg_2015",
+               "bcg_2016", "rate_birth_2014", "rate_birth_2015", "life_expectancy_2014", "life_expectancy_2015",
+               "notifications_2014", "notifications_2015", "notifications_2016", "outcomes_2013", "outcomes_2015",
+               "mdr_2014", "mdr_2015", "mdr_2016", "laboratories_2014", "laboratories_2015", "laboratories_2016",
+               "strategy_2014", "strategy_2015", "strategy_2016", "diabetes", "gtb_2015", "gtb_2016", "latent_2016",
+               "tb_hiv_2016", "spending_inputs", "constants", "time_variants"]
+
+        for filename in excel_file_list:
+            xls = pd.ExcelFile(filename)
+
+            # for single work sheets
+            if len(xls.sheet_names) == 1:
+                df_name = xls.sheet_names[0]
+                df = pd.read_excel(filename, sheet_name=df_name)
+                df.to_sql(df_name, con=self.engine, if_exists="replace")
+                print(df_name)
+            else:
+                n_sheets = 0
+                while n_sheets < len(xls.sheet_names):
+                    sheet_name = xls.sheet_names[n_sheets]
+                    if sheet_name in available_sheets:
+
+                        # set headers for rate_birth_2015 and life_expectancy_2015
+                        if sheet_name == "rate_birth_2015" or sheet_name == "life_expectancy_2015" :
+                            df = pd.read_excel(filename, sheet_name=sheet_name, header=3)
+                        else:
+                            df = pd.read_excel(filename, sheet_name=sheet_name)
+                        print(sheet_name)
+
+                        # to read constants and time variants
+                        if sheet_name == "constants":
+                            sheet_name = filename.replace(".xlsx", "").split("_")[1] + "_constants"
+                        if sheet_name == "time_variants":
+                            sheet_name = filename.replace(".xlsx", "").split("_")[1] + "_time_variants"
+                        df.to_sql(sheet_name, con=self.engine, if_exists="replace")
+                    n_sheets += 1
+
+    def db_query(self, table_name, filter="", value="", column="*"):
+        """
+        method to query table_name
+        """
+        query = "Select " + column + " from  " + table_name
+        if filter != "" and value != "":
+            query = "Select " + column + " from  " + table_name + " Where " + filter + " = \'" + value + "\'"
+        output_from_db = pd.read_sql_query(query, con=self.engine)
+        return output_from_db
+
+
+if __name__ == "__main__":
+
+    input = InputDB()
+    input.load_xlsx()
+    input.load_csv()
+    res = input.db_query("bcg_2015", filter="Cname", value="Bhutan")
+    print(res)
+    res = input.db_query("notifications_2016", filter="Country", value="Bhutan")
+    print(res)
+    res = input.db_query("default_time_variants", filter="program", value="econ_cpi")
+    print(res)
+    res = input.db_query("bhutan_constants", filter="parameter", value="tb_n_contact")
+    print(res)
+    res = input.db_query("bhutan_time_variants", filter="program", value="int_perc_firstline_dst")
+    print(res.values)
---
+++ tb_model.py
@@ -0,0 +1,138 @@
+
+import summer_model
+import matplotlib.pyplot
+import os
+
+
+def add_vtp_latency_parameters(parameters_, change_time_unit=365.25):
+    """
+    function to add the latency parameters estimated by Ragonnet et al from our paper in Epidemics to the existing
+    parameter dictionary
+    """
+    vtp_latency_parameters = \
+        {"early_progression": 1.1e-3,
+         "stabilisation": 1.0e-2,
+         "late_progression": 5.5e-6}
+    parameters_.update({key: value * change_time_unit for key, value in vtp_latency_parameters.items()})
+    return parameters_
+
+
+def get_age_specific_latency_parameters(parameter, unit_change=365.25):
+    """
+    get the age-specific latency parameters estimated by Ragonnet et al
+    """
+    age_stratified_parameters = \
+        {"early_progression": {"0W": 6.6e-3, "5W": 2.7e-3, "15W": 2.7e-4},
+         "stabilisation": {"0W": 1.2e-2, "5W": 1.2e-2, "15W": 5.4e-3},
+         "late_progression": {"0W": 1.9e-11, "5W": 6.4e-6, "15W": 3.3e-6}}
+    return {key: value * unit_change for key, value in age_stratified_parameters[parameter].items()}
+
+
+def get_all_age_specific_latency_parameters(parameters_=("early_progression", "stabilisation", "late_progression")):
+    """
+    collate all the latency parameters together from the previous function
+    """
+    return {parameter: get_age_specific_latency_parameters(parameter) for parameter in parameters_}
+
+
+def add_standard_latency_flows(flows_):
+    """
+    adds our standard latency flows to the list of flows to be implemented in the model
+    """
+    flows_ += [
+        {"type": "standard_flows", "parameter": "early_progression", "origin": "early_latent", "to": "infectious"},
+        {"type": "standard_flows", "parameter": "stabilisation", "origin": "early_latent", "to": "late_latent"},
+        {"type": "standard_flows", "parameter": "late_progression", "origin": "late_latent", "to": "infectious"}]
+    return flows_
+
+
+def sinusoidal_scaling_function(start_time, baseline_value, end_time, final_value):
+    """
+    with a view to implementing scale-up functions over time, use the cosine function to produce smooth scale-up
+    functions from one point to another
+    """
+    def sinusoidal_function(x):
+        if not isinstance(x, float):
+            raise ValueError("value fed into scaling function not a float")
+        elif start_time > end_time:
+            raise ValueError("start time is later than end time")
+        elif x < start_time:
+            return baseline_value
+        elif start_time < x < end_time:
+            return baseline_value + \
+                   (final_value - baseline_value) * \
+                   (0.5 - 0.5 * numpy.cos((x - start_time) * numpy.pi / (end_time - start_time)))
+        else:
+            return final_value
+    return sinusoidal_function
+
+
+def convert_competing_proportion_to_rate(competing_flows):
+    """
+    convert a proportion to a rate dependent on the other flows coming out of a compartment
+    """
+    return lambda proportion: proportion * competing_flows / (1.0 - proportion)
+
+
+def return_function_of_function(inner_function, outer_function):
+    """
+    general method to return a chained function from two functions
+    """
+    return lambda value: outer_function(inner_function(value))
+
+
+if __name__ == "__main__":
+
+    # set basic parameters, flows and times, except for latency flows and parameters, then functionally add latency
+    case_fatality_rate = 0.4
+    untreated_disease_duration = 3.0
+    parameters = \
+        {"beta": 10.0,
+         "recovery": case_fatality_rate / untreated_disease_duration,
+         "infect_death": (1.0 - case_fatality_rate) / untreated_disease_duration,
+         "universal_death_rate": 1.0 / 50.0,
+         "case_detection": 0.0}
+    parameters = add_vtp_latency_parameters(parameters)
+
+    times = numpy.linspace(1800., 2020.0, 201).tolist()
+    flows = [{"type": "infection_frequency", "parameter": "beta", "origin": "susceptible", "to": "early_latent"},
+             {"type": "infection_frequency", "parameter": "beta", "origin": "recovered", "to": "early_latent"},
+             {"type": "standard_flows", "parameter": "recovery", "origin": "infectious", "to": "recovered"},
+             {"type": "compartment_death", "parameter": "infect_death", "origin": "infectious"}]
+    flows = add_standard_latency_flows(flows)
+
+    tb_model = summer_model.StratifiedModel(
+        times, ["susceptible", "early_latent", "late_latent", "infectious", "recovered"], {"infectious": 1e-3},
+        parameters, flows, birth_approach="replace_deaths")
+
+    tb_model.add_transition_flow(
+        {"type": "standard_flows", "parameter": "case_detection", "origin": "infectious", "to": "recovered"})
+
+    cdr_scaleup = sinusoidal_scaling_function(1950.0, 0.0, 2010.0, 0.6)
+    prop_to_rate = convert_competing_proportion_to_rate(1.0 / untreated_disease_duration)
+    detect_rate = return_function_of_function(cdr_scaleup, prop_to_rate)
+
+    tb_model.time_variants["case_detection"] = detect_rate
+
+    print(get_all_age_specific_latency_parameters())
+    tb_model.stratify("age", [5, 15], [],
+                      adjustment_requests=get_all_age_specific_latency_parameters(),
+                      report=False)
+    tb_model.run_model()
+
+    print(os.getcwd())
+    # tb_model.transition_flows.to_csv("_.csv")
+
+    # get outputs
+    infectious_population = tb_model.outputs[:, tb_model.compartment_names.index("infectiousXage_0")] + \
+                            tb_model.outputs[:, tb_model.compartment_names.index("infectiousXage_5")] + \
+                            tb_model.outputs[:, tb_model.compartment_names.index("infectiousXage_15")]
+
+    matplotlib.pyplot.plot(times, infectious_population * 1e5)
+    # print(infectious_population * 1e5)
+
+    # tb_model.death_flows.to_csv("tb_model_deaths.csv")
+
+    matplotlib.pyplot.xlim((1950., 2010.))
+    matplotlib.pyplot.ylim((0.0, 2000.0))
+    # matplotlib.pyplot.show()
