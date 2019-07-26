c7ac7b30e821748416f317134eb317bef507d6de
James Trauer <james.trauer@monash.edu>
2019-07-26 16:08:55 +1000

    not much progress - but something to go on with later

--- python_source_code/summer_model.py
+++ python_source_code/summer_model.py
@@ -161,7 +161,6 @@ def logistic_scaling_function(parameter):
     """
     def sigmoidal_function(x):
         return 1.0 - 1.0 / (1.0 + numpy.exp(-(parameter - x)))
-
     return sigmoidal_function
 
 
@@ -268,6 +267,18 @@ def create_flowchart(model_object, strata=-1, stratify=True, name="flow_chart"):
         model_object.flow_diagram.edge(row[1]["origin"], row[1]["to"], row[1]["parameter"])
     model_object.flow_diagram = apply_styles(model_object.flow_diagram, styles)
     model_object.flow_diagram.render(name)
+
+
+def create_multiplicative_function(multiplier):
+    def multiplicative_function(value):
+        return value * multiplier
+    return multiplicative_function
+
+
+def create_function_of_function(outer_function, inner_function):
+    def function_to_return():
+        return outer_function(inner_function())
+    return function_to_return
 
 
 class EpiModel:
@@ -1492,6 +1503,7 @@ class StratifiedModel(EpiModel):
 
         # and adjust
         self.ordered_parameter_adjustments = {}
+        self.parameter_functions = {}
         for parameter in parameters_to_adjust:
             self.find_parameter_components(parameter)
             self.find_parameter_components_shadow(parameter)
@@ -1531,17 +1543,36 @@ class StratifiedModel(EpiModel):
 
     def find_parameter_components_shadow(self, _parameter):
         self.ordered_parameter_adjustments[_parameter] = []
-        for x_instance in extract_reversed_x_positions(_parameter):
+        for n_x_instance, x_instance in enumerate(extract_x_positions(_parameter)):
             component = _parameter[: x_instance]
             self.ordered_parameter_adjustments[_parameter].append(component)
+            if n_x_instance == 0:
+                # print(component)
+                # print(self.parameters[component])
+                def return_starting_parameter_value():
+                    return self.parameters[component]
+                # print(return_starting_parameter_value())
+                self.parameter_functions[_parameter] = return_starting_parameter_value
+            else:
+                self.parameter_functions[_parameter] = create_function_of_function(
+                    create_multiplicative_function(self.parameters[component]), self.parameter_functions[_parameter])
             if component in self.overwrite_parameters:
                 break
+        print(self.ordered_parameter_adjustments[_parameter])
 
     """
     methods to be called during the process of model running
     """
 
     def get_parameter_value(self, _parameter, _time):
+
+        print("______")
+        print(_parameter)
+        self.get_parameter_value_1(_parameter, _time)
+        adjusted_parameter = self.get_parameter_value_2(_parameter, _time)
+        return adjusted_parameter
+
+    def get_parameter_value_1(self, _parameter, _time):
         """
         using the approach specified in find_parameter_components calculate adjusted parameter value from pre-calculated
         product of constant components and time variants
@@ -1554,12 +1585,23 @@ class StratifiedModel(EpiModel):
         adjusted_parameter = self.parameter_components[_parameter]["constant_value"]
         for time_variant in self.parameter_components[_parameter]["time_variants"]:
             adjusted_parameter *= self.time_variants[time_variant](_time)
-        return adjusted_parameter
+        # print(adjusted_parameter)
+        # return adjusted_parameter
 
-    def get_parameter_value_shadow(self, _parameter, _time):
-        adjusted_parameter = self.parameters[_parameter]
+    def get_parameter_value_2(self, _parameter, _time):
+        adjusted_parameter = self.parameters[self.ordered_parameter_adjustments[_parameter][0]]
         for component in self.ordered_parameter_adjustments[_parameter][1:]:
             adjusted_parameter *= self.parameters[component]
+        # print(adjusted_parameter)
+        # # return adjusted_parameter
+        #
+        # if callable(self.parameter_functions[_parameter]):
+        #     adjusted_parameter = self.parameter_functions[_parameter]()
+        # else:
+        #     adjusted_parameter = self.parameter_functions[_parameter]
+        #
+        # print(adjusted_parameter)
+
         return adjusted_parameter
 
     def find_infectious_population(self, _compartment_values):
