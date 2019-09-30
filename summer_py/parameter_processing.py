94a4b266dedcbf42f9d6cf1c58681fe8ebcc5409
James Trauer <james.trauer@monash.edu>
2019-09-30 15:28:48 +1000

    add new file to contain parameter processing functions

--- summer_py/parameter_processing.py
+++ summer_py/parameter_processing.py
@@ -12,3 +12,14 @@ def change_parameter_unit(parameter_dict, multiplier):
         dictionary with values multiplied by the multiplier argument
     """
     return {param_key: param_value * multiplier for param_key, param_value in parameter_dict.items()}
+
+
+def add_w_to_param_names(parameter_dict):
+    """
+    add a "W" string to the end of the parameter name to indicate that we should over-write up the chain
+    :param parameter_dict: dict
+        the dictionary before the adjustments
+    :return: dict
+        same dictionary but with the "W" string added to each of the keys
+    """
+    return {str(age_group) + "W": value for age_group, value in parameter_dict.items()}
