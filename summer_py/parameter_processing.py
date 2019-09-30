e89f18c05e7ed120a4f9c9afc6dcfdebf5574a97
James Trauer <james.trauer@monash.edu>
2019-09-30 15:30:42 +1000

    add another function to new parameter processing file

--- summer_py/parameter_processing.py
+++ summer_py/parameter_processing.py
@@ -23,3 +23,27 @@ def add_w_to_param_names(parameter_dict):
         same dictionary but with the "W" string added to each of the keys
     """
     return {str(age_group) + "W": value for age_group, value in parameter_dict.items()}
+
+
+def create_step_function_from_dict(input_dict):
+    """
+    create a step function out of dictionary with numeric keys and values, where the keys determine the values of the
+    independent variable at which the steps between the output values occur
+    :param input_dict: dict
+        dictionary in standard format as described above
+    :return: function
+        the function constructed from input data
+    """
+    dict_keys = list(input_dict.keys())
+    dict_keys.sort()
+    dict_values = [input_dict[key] for key in dict_keys]
+
+    def step_function(input_value):
+        if input_value >= dict_keys[-1]:
+            return dict_values[-1]
+        else:
+            for key in range(len(dict_keys)):
+                if input_value < dict_keys[key + 1]:
+                    return dict_values[key]
+
+    return step_function
